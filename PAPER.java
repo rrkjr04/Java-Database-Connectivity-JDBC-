c110b00a11dc82fbae2d892c47b7ff823b16be7d
Muhammad Rashid Khan <128826588+rrkjr04@users.noreply.github.com>
2023-04-01 21:31:53 +0500

    Update PAPER.java

--- PAPER.java
+++ PAPER.java
@@ -6,18 +6,18 @@ class SmartPhone1{
     public String model;
     public String cname;
 
-    SmartPhone1(String model,String cname)
+    SmartPhone1(String model,String cname)          //Construtctor to initialize values to attributes
     {
         this.model = model;
         this.cname = cname;
     }
 
-    ArrayList<SmartPhone1> arrli = new ArrayList<SmartPhone1>();
+    ArrayList<SmartPhone1> arrli = new ArrayList<SmartPhone1>();            //An ArrayList to store values coming from database
 
     public ArrayList<SmartPhone1>allData()
     {
         try{
-             Class.forName("com.mysql.cj.jdbc.Driver");
+             Class.forName("com.mysql.cj.jdbc.Driver");         //Old term com.mysql.jdbc.Driver is no longer available
             Connection con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","");
             Statement stm = con.createStatement();
             ResultSet rs = stm.executeQuery("SELECT * FROM smartphone");
@@ -38,7 +38,7 @@ class SmartPhone1{
     }
 }
 public class PAPER extends SmartPhone1 {
-    PAPER(String un,String vn)
+    PAPER(String un,String vn)                  //Constructor is made because if you are inheriting a class you have to make a constructor with same arguements in child class as it is present in base class
     {
         super(un,vn);
     }
