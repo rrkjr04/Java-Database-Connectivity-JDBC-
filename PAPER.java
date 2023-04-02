7b0a67571d4e88368f067bda188373e2567ca047
Ali Gates <128673394+AliGates915@users.noreply.github.com>
2023-04-02 10:07:57 +0500

    Your code is awesome, GLTM

--- PAPER.java
+++ PAPER.java
@@ -12,12 +12,12 @@ class SmartPhone1{
         this.cname = cname;
     }
 
-    ArrayList<SmartPhone1> arrli = new ArrayList<SmartPhone1>();            //An ArrayList to store values coming from database
+    ArrayList<SmartPhone1> arrli = new ArrayList<SmartPhone1>();         //An ArrayList to store values coming from database
 
-    public ArrayList<SmartPhone1>allData()
+    public ArrayList<SmartPhone1> allData()
     {
         try{
-             Class.forName("com.mysql.cj.jdbc.Driver");         //Old term com.mysql.jdbc.Driver is no longer available
+             Class.forName("com.mysql.cj.jdbc.Driver");     //Old term com.mysql.jdbc.Driver is no longer available
             Connection con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","");
             Statement stm = con.createStatement();
             ResultSet rs = stm.executeQuery("SELECT * FROM smartphone");
@@ -38,11 +38,10 @@ class SmartPhone1{
     }
 }
 public class PAPER extends SmartPhone1 {
-    PAPER(String un,String vn)                  //Constructor is made because if you are inheriting a class you have to make a constructor with same arguements in child class as it is present in base class
+    PAPER(String un,String vn)              //Constructor is made because if you are inheriting a class you have to make a constructor with same arguements in child class as it is present in base class
     {
         super(un,vn);
     }
-
 
 
     public static void main(String[] args) {
@@ -50,8 +49,8 @@ public class PAPER extends SmartPhone1 {
         ArrayList<SmartPhone1> arli1 = obj.allData();
         for(int i=0;i<arli1.size();i++)
         {
-            System.out.println(arli1.get(i).model+" "+arli1.get(i).cname);
+            System.out.println(arli1.get(i).model+" \t "+arli1.get(i).cname);
         }
-
+        System.out.println("Successfully ! Access Data From smartphone SQL Database");
     }
 }
